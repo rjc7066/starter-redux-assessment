a2106adac65d5436dac06a937a56d788c28a0d6a
Reed <reedjcutter@gmail.com>
2024-01-23 16:31:10 -0600

    Completed assessment

--- src/features/photos/create/index.js
+++ src/features/photos/create/index.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addPhoto } from '../photos.slice';
 
 // Task 2: Import the `useDispatch()` method from the appropriate package
 // Task 3: Import the `addPhoto()` action creator from the photos slice
@@ -8,6 +10,7 @@ import './create.css';
 export default function CreatePhoto() {
   const [formData, setFormData] = useState({ imageUrl: '', caption: '' });
   // Task 4: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch();
 
   function handleChange({ target: { name, value } }) {
     setFormData({
@@ -20,6 +23,7 @@ export default function CreatePhoto() {
     event.preventDefault();
     // Task 5: Dispatch the `addPhoto()` action creator, passing in the form data
     setFormData({ imageUrl: '', caption: '' });
+    dispatch(addPhoto(formData));
   }
 
   return (
